bbf81dd2e58d699538318278b786577d4f5016ec
AndreyLP <pifagor174@yandex.ru>
2026-03-14 21:58:54 +0300

    Best Time to Buy and Sell Stock

---
+++ tests/Array.Tests/BuySellStockTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+
+namespace Array.Tests
+{
+    public class BuySellStockTests
+    {
+        [InlineData(new int[6] { 7, 1, 5, 3, 6, 4 }, 5)]
+        [InlineData(new int[5] { 7, 6, 4, 3, 1 }, 0)]
+        [Theory]
+        public void CheckGetProfit(int[] nums, int majority)
+        {
+            BuySellStock sut = new();
+
+            var result = sut.MaxProfit(nums);
+
+            result.Should().Be(majority);
+        }
+    }
+}
