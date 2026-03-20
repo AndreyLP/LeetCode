de10905ae73d18dbf7479cc8d41a64fbb0084a82
AndreyLP <pifagor174@yandex.ru>
2026-03-20 14:33:23 +0300

    13. Roman to Integer

---
+++ tests/LeetCode.Tests/ArrayTests.cs
@@ -0,0 +1,21 @@
+﻿using Array;
+using FluentAssertions;
+
+namespace LeetCode.Tests
+{
+    public class ArrayTests
+    {
+        [InlineData("III", 3)]
+        [InlineData("LVIII", 58)]
+        [InlineData("MCMXCIV", 1994)]
+        [Theory]
+        public void CheckRomanToInt(string romanNumeral, int IntNumeral)
+        {
+            var sut = new RomanToInteger();
+
+            var result = sut.RomanToInt(romanNumeral);
+
+            result.Should().Be(IntNumeral);
+        }
+    }
+}
