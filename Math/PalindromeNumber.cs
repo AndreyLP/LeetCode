203365039b4f023528fc64e957e1d26f230e08b1
AndreyLP <pifagor174@yandex.ru>
2026-03-23 23:13:04 +0300

    9. Palindrome Number

---
+++ Math/PalindromeNumber.cs
@@ -0,0 +1,51 @@
+﻿namespace Math
+{
+    public class PalindromeNumber
+    {
+        //9. Palindrome Number
+        //Given an integer x, return true if x is a palindrome, and false otherwise.
+
+        //Example 1:
+
+        //Input: x = 121
+        //Output: true
+        //Explanation: 121 reads as 121 from left to right and from right to left.
+        //Example 2:
+
+        //Input: x = -121
+        //Output: false
+        //Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+        //Example 3:
+
+        //Input: x = 10
+        //Output: false
+        //Explanation: Reads 01 from right to left.Therefore it is not a palindrome.
+
+
+        //Constraints:
+
+        //-2^31 <= x <= 2^31 - 1
+
+
+
+        //Follow up: Could you solve it without converting the integer to a string?
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0 || (x % 10 == 0 && x != 0))
+            {
+                return false;
+            }
+
+            var reversed = 0;
+            var curX = x;
+
+            while (curX > reversed)
+            {
+                reversed = reversed * 10 + curX % 10;
+                curX /= 10;
+            }
+
+            return reversed == curX || reversed / 10 == curX;
+        }
+    }
+}
---
+++ tests/LeetCode.Tests/Math.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Math;
+
+namespace LeetCode.Tests
+{
+    public class Math
+    {
+        [InlineData(121,true)]
+        [InlineData(-121, false)]
+        [InlineData(10, false)]
+        [Theory]
+        public void IsPalindrome(int x, bool isCorrect)
+        {
+            var sut = new PalindromeNumber();
+
+            var result = sut.IsPalindrome(x);
+
+            result.Should().Be(isCorrect);
+        }
+    }
+}
